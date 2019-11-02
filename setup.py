72e804371bd7bf73f738055de3b3f6b38415e96b
DDL <dli.stats@gmail.com>
2019-11-02 03:13:34 -0400

    test out setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='varderiv',
     version='0.1dev',
-    py_modules=['varderiv'],
+    package_dir={'': 'varderiv'},
+    packages=[''],
     license='MIT license',
     long_description=open('README.md').read(),
 )
