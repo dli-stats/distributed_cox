4a35ddfe69f7fe4955184d7acc6247b5a2d527ee
DDL <dli.stats@gmail.com>
2019-11-02 01:09:20 -0400

    add colab notebook and packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='varderiv',
+    version='0.1dev',
+    packages=['varderiv',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+)
