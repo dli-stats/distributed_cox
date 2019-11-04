1d9145e5784062ea41bcc7e8c1cfd69001e5ab3a
DDL @ slurm <dl263@compute-g-16-254.o2.rc.hms.harvard.edu>
2019-11-04 18:19:23 -0500

    add slurm scripts

---
+++ scripts/slurm/run_experiments_A.sh
@@ -0,0 +1,31 @@
+run() {
+  for X_DIM in 2 3 5; do
+    for N in 200 300 500 1000; do
+        sbatch single.sh $1 " \
+          with \
+            base.num_experiments=100000 \
+            base.seed=0 \
+            base.X_DIM=$X_DIM \
+            base.N=$N \
+            $2 \
+        "
+    done
+  done
+}
+
+EQ=$1
+
+if [ "$EQ" == "eq2" ] || [ "$EQ" == "eq4" ] ; then
+  BATCH_ARG="base.batch_size=32"
+else
+  BATCH_ARG="base.batch_size=128"
+fi
+
+if [ "$EQ" == "eq1" ]; then
+  run $EQ
+else
+  for K in 3 4 5; do
+    run $EQ "grouped.K=${K} ${BATCH_ARG}"
+  done
+fi
+
