076d8aa5b3df4a6e1d3d7bdf0f4602826f7a87a9
DDL <dli.stats@gmail.com>
2019-11-04 18:32:53 -0500

    round group size to 10

--- varderiv/data.py
+++ varderiv/data.py
@@ -198,8 +198,7 @@ def group_data_by_labels(batch_size, K, X, delta, group_labels):
       [[group_labels[i] == k for k in range(K)] for i in range(batch_size)])
 
   padded_group_size = onp.max(onp.sum(group_mask, axis=(-1,)))
-
-  # padded_group_size = int(math.ceil(padded_group_size / 10)) * 10
+  padded_group_size = int(math.ceil(padded_group_size / 10)) * 10
 
   all_X_groups = [None] * batch_size
   all_delta_groups = [None] * batch_size
