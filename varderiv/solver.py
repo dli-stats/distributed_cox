0638295ee762416e6e88412a13e23b9c2c973479
DDL <dli.stats@gmail.com>
2019-11-06 01:40:08 -0500

    revert back nan check

--- varderiv/solver.py
+++ varderiv/solver.py
@@ -1,7 +1,6 @@
 """Newton solver."""
 
 import collections
-import functools
 
 import jax.lax
 import jax.numpy as np
@@ -28,50 +27,37 @@ def solve_newton(fn,
   else:
     jac_fn = jacrev(fn, 0)
 
-  norm_fn = functools.partial(np.linalg.norm, ord=np.inf)
-
   initial_value = fn(initial_guess)
-  initial_norm = norm_fn(initial_value)
 
   InternalState = collections.namedtuple("InternalState",
-                                         "guess value norm step key")
-
-  def reset(state: InternalState):
-    key, subkey = jrandom.split(state.key, 2)
-    guess = initial_guess + jrandom.normal(subkey, shape=initial_guess.shape)
-    value = fn(guess)
-    norm = norm_fn(value)
-    return InternalState(guess, value, norm, state.step, key)
+                                         "guess value step key")
 
   def newton_update(state: InternalState):
 
-    guess, value, _, step, key = state
-    jacobian = jac_fn(guess)
-    new_guess = guess + scipy.linalg.solve(-jacobian, value, sym_pos=sym_pos)
-    new_value = fn(new_guess)
-    new_norm = norm_fn(new_value)
-    new_step = step + 1
-    new_state = InternalState(guess=new_guess,
-                              value=new_value,
-                              norm=new_norm,
-                              step=new_step,
-                              key=key)
+    def reset(state: InternalState):
+      key, subkey = jrandom.split(state.key, 2)
+      guess = initial_guess + jrandom.normal(subkey, shape=initial_guess.shape)
+      value = fn(guess)
+      return InternalState(guess, value, step + 1, key)
 
-    return jax.lax.cond(
-        np.logical_or(new_step % max_reset_steps == 0, np.isnan(new_norm)),
-        new_state,
-        reset,  # reset if exceeded steps
-        new_state,
-        lambda s: s  # else return new_state
-    )
+    def update(state: InternalState):
+      guess, value, step, key = state
+      jacobian = jac_fn(guess)
+      guess = guess + scipy.linalg.solve(-jacobian, value, sym_pos=sym_pos)
+      value = fn(guess)
+      return InternalState(guess, value, step + 1, key)
 
-  def loop_cond(state):
-    return np.logical_and(state.step < max_num_steps,
-                          state.norm > norm_stop_thres)
+    step = state.step
+    return jax.lax.cond(((step + 1) % max_reset_steps == 0), state, reset,
+                        state, update)
 
-  ret = jax.lax.while_loop(
-      loop_cond, newton_update,
-      InternalState(initial_guess, initial_value, initial_norm, 0, key))
+  def stop_cond(state):
+    return np.logical_and(
+        state.step < max_num_steps,
+        np.linalg.norm(state.value, ord=np.inf) > norm_stop_thres)
+
+  ret = jax.lax.while_loop(stop_cond, newton_update,
+                           InternalState(initial_guess, initial_value, 0, key))
 
   return NewtonSolverState(ret.guess, ret.value, ret.step)
 
