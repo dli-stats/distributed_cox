e733955bb751191935db9ed1d6c56b87fda301a1
dli-stats <dli.stats@gmail.com>
2021-01-28 16:00:01 -0500

    some cleanup

--- scripts/slurm/additional.py
+++ scripts/slurm/additional.py
@@ -7,6 +7,16 @@ import textwrap
 
 import simple_slurm
 
+slurm = simple_slurm.Slurm(cpus_per_task=8,
+                           nodes=1,
+                           time='0-2:00',
+                           partition='short',
+                           mem=3000,
+                           output='hostname_%j.out',
+                           error='hostname_%j.err',
+                           mail_type='FAIL',
+                           mail_user='dl263@hms.harvard.edu')
+
 Param = collections.namedtuple("Param", "N K nk p")
 settings = [
     ## Plan 1
@@ -55,16 +65,6 @@ root_dir = subprocess.check_output(["git", "rev-parse",
                                     "--show-toplevel"]).strip().decode("utf-8")
 storage_dir = "/n/scratch3/users/d/dl263/varderiv_experiments"
 
-slurm = simple_slurm.Slurm(cpus_per_task=8,
-                           nodes=1,
-                           time='0-2:00',
-                           partition='short',
-                           mem=3000,
-                           output='hostname_%j.out',
-                           error='hostname_%j.err',
-                           mail_type='FAIL',
-                           mail_user='dl263@hms.harvard.edu')
-
 batch_size = 32
 T_star_factorss = ["None"]
 eqs = ["eq1", "eq2", "eq3", "eq4", "meta_analysis", "meta_analysis_univariate"]
@@ -76,16 +76,16 @@ for (eq, (N, K, nk, p),
   else:
     meta_analysis_univariate = False
   x_dim = p.count(";") + 1
-  p = p.replace(";", ",").replace("Ber", "bernoulli").replace("N", "normal")
+  p = p.replace(";", ",").replace(" ", "")
+  p = p.replace("Ber", "bernoulli").replace("N", "normal")
+
   slurm.sbatch(
       textwrap.dedent(f"""
         ROOT_DIR={root_dir}
         source activate varderiv
         cd $ROOT_DIR
 
-        python -m distributed_cox.experiments.run -p
-        -F {storage_dir}
-        with \\
+        python -m distributed_cox.experiments.run -p -F {storage_dir} with \\
           num_experiments=10000 \\
           eq={eq} \\
           batch_size={batch_size} \\
