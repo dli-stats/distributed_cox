fix distributed eq4 group_correction covariance bug

distributed_cox/distributed/eq4.py
@@ -17,7 +17,7 @@ import distributed_cox.distributed.eq2 as eq2
 def eq4_local(local_state: ClientState,
               initial_guess: Optional[List[float]] = None,
               loglik_eps: float = 1e-6,
-              max_num_steps: int = 30) -> Message:
+              max_num_steps: int = 200) -> Message:
   T_group, delta_group = local_state.get_vars("T_group", "delta_group")
   local_state["T_delta"] = T_group[delta_group]
   msg = eq2.eq2_local(local_state,
@@ -28,6 +28,7 @@ def eq4_local(local_state: ClientState,
     # Under cox correction there's one more round of communication
     # So that all the variance estimations are done locally
     del msg["nxebkx_cs_ds{}".format(local_state.config.taylor_order + 2)]
+
   return msg
 
 
@@ -60,12 +61,12 @@ def _eq4_model(X_delta_sum, nxebkx_cs_ds, beta_k_hat, beta, taylor_order=1):
 
 def solve_eq4_model(master_state: ClientState,
                     score_norm_eps: float = 1e-3,
-                    max_num_steps: int = 30):
-  """Solves eq2_model."""
+                    max_num_steps: int = 100):
+  """Solves eq4_model."""
   X_delta_sum, nxebkx_cs_ds, beta_k_hat, group_sizes = master_state.get_vars(
       "X_delta_sum", "nxebkx_cs_ds*", "beta_k_hat", "group_sizes")
-  beta_k_hat = np.repeat(beta_k_hat, group_sizes, axis=0)
   beta_guess = np.mean(beta_k_hat, axis=0)
+  beta_k_hat = np.repeat(beta_k_hat, group_sizes, axis=0)
   fn_to_solve = functools.partial(_eq4_model,
                                   X_delta_sum,
                                   nxebkx_cs_ds,
@@ -77,8 +78,9 @@ def solve_eq4_model(master_state: ClientState,
                                max_num_steps=max_num_steps)(beta_guess)
 
 
-def eq4_master(master_state: ClientState, score_norm_eps=1e-3,
-               max_num_steps=30) -> Message:
+def eq4_master(master_state: ClientState,
+               score_norm_eps=1e-3,
+               max_num_steps=1000) -> Message:
   nxebkx_cs_ds, = master_state.get_vars("nxebkx_cs_ds*")
   master_state["group_sizes"] = np.array(list(map(len, nxebkx_cs_ds[0])))
   nxebkx_cs_ds_concat = tuple(np.concatenate(x, axis=0) for x in nxebkx_cs_ds)
@@ -89,6 +91,10 @@ def eq4_master(master_state: ClientState, score_norm_eps=1e-3,
                         max_num_steps=max_num_steps)
 
   beta_hat = sol.guess
+  if not sol.converged:
+    print(f"Did not converge in {max_num_steps} steps!")
+
+  master_state["converged"] = sol.converged
   master_state["beta_hat"] = beta_hat
   master_state[str(VarianceSetting(False, False,
                                    False))] = modeling.cov_H()(sol)
@@ -104,18 +110,19 @@ def eq4_master(master_state: ClientState, score_norm_eps=1e-3,
 
 
 def eq4_local_variance(local_state: ClientState) -> Message:
-  (beta_k_hat, beta_hat, X_group,
-   delta_group) = local_state.get_vars("beta_k_hat", "beta_hat", "X_group",
-                                       "delta_group")
+  (beta_k_hat, beta_hat, X_group, delta_group, T_group,
+   T_delta) = local_state.get_vars("beta_k_hat", "beta_hat", "X_group",
+                                   "delta_group", "T_group", "T_delta")
   taylor_order = local_state.config.taylor_order
   # Obtain the denom and numers for the current group
   nxebkxs = eq2.compute_nxebkxs(X_group,
                                 beta_k_hat,
                                 required_orders=taylor_order + 2)
-  nxebkx_cs_ds = [np.cumsum(nxebkx, 0) for nxebkx in nxebkxs]
+  nxebkx_css = [np.cumsum(nxebkx, 0) for nxebkx in nxebkxs]
+
   denom, numer = collect(_eq4_model, ["denom", "numer"])(
       np.zeros_like(beta_k_hat).reshape((1, -1)),  # argument not used
-      nxebkx_cs_ds,
+      nxebkx_css,
       np.tile(beta_k_hat, (X_group.shape[0], 1)),
       beta_hat,
       taylor_order=taylor_order,
@@ -143,6 +150,10 @@ def eq4_local_variance(local_state: ClientState) -> Message:
                       local_state.config.require_group_correction,
                       local_state.config.require_robust))
 
+  idxs = np.searchsorted(-T_group, -T_delta, side='right')
+  nxebkx_cs_ds = tuple(
+      np.take(nxebkx_cs, idxs, axis=0) for nxebkx_cs in nxebkx_css)
+
   grad_beta_k = _compute_eq4_grad_beta_k(nxebkx_cs_ds, beta_k_hat, beta_hat,
                                          taylor_order)
   msg['grad_beta_k'] = grad_beta_k
@@ -163,7 +174,7 @@ def _compute_eq4_grad_beta_k(nxebkx_cs_ds, beta_k_hat, beta, taylor_order):
                                           beta,
                                           taylor_order=taylor_order)
   dbeta_pow_t = dbeta_pow_t[0]
-  t1ebkxbmb_cs_d = np.einsum("dip,p->dp", t1xebkx_cs_ds.reshape((D, X_dim, -1)),
+  t1ebkxbmb_cs_d = np.einsum("dip,p->di", t1xebkx_cs_ds.reshape((D, X_dim, -1)),
                              dbeta_pow_t)
   t2ebkxbmb_cs_d = np.einsum("dijp,p->dij",
                              t2xebkx_cs_ds.reshape((D, X_dim, X_dim, -1)),
@@ -178,10 +189,19 @@ def _compute_eq4_grad_beta_k(nxebkx_cs_ds, beta_k_hat, beta, taylor_order):
 
 def _get_or_compute_eq2_grad_beta_k(master_state: ClientState):
   if "grad_beta_k" not in master_state:
-    return _compute_eq4_grad_beta_k(
-        *master_state.get_vars("nxebkx_cs_ds*", "beta_k_hat", "beta_hat"),
-        master_state.config.taylor_order)
-  return master_state["grad_beta_k"]
+    nxebkx_cs_ds, beta_k_hat, beta_hat, group_sizes = master_state.get_vars(
+        "nxebkx_cs_ds*", "beta_k_hat", "beta_hat", "group_sizes")
+    idx = 0
+    grad_beta_k = []
+    for i, group_size in enumerate(group_sizes):
+      nxebkx_cs_ds_group = [n[idx:idx + group_size] for n in nxebkx_cs_ds]
+      idx += group_size
+      grad_beta_k_group = _compute_eq4_grad_beta_k(
+          nxebkx_cs_ds_group, beta_k_hat[i], beta_hat,
+          master_state.config.taylor_order)
+      grad_beta_k.append(grad_beta_k_group)
+    return np.stack(grad_beta_k)
+  return master_state.get_var("grad_beta_k")
 
 
 def _get_B(state, *B_names, cox_correction=False):
@@ -206,7 +226,7 @@ def _eq4_master_variance(master_state: ClientState,
           "B_diag_last",
           "B_row_wo_last",
           cox_correction=variance_setting.cox_correction)
-      S = np.einsum("ab,bBc,Bcd->Bad", I_diag_inv_last, I_row_wo_last,
+      S = np.einsum("ab,Bbc,Bcd->Bad", I_diag_inv_last, I_row_wo_last,
                     I_diag_inv_wo_last)
 
       sas = np.einsum("Bab,Bbc,Bdc->ad", S, B_diag_wo_last, S)
@@ -216,7 +236,7 @@ def _eq4_master_variance(master_state: ClientState,
                       I_diag_inv_last)
       cov = sas - sb1s - sb2s + scs
     else:
-      S = np.einsum("ab,bBc->Bac", I_diag_inv_last, I_row_wo_last)
+      S = np.einsum("ab,Bbc->Bac", I_diag_inv_last, I_row_wo_last)
 
       cov = np.einsum(
           "Bab,Bbc,Bdc->ad", S, I_diag_inv_wo_last, S,